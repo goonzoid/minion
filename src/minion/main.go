8239bb7ac50e32f7bd6f19206d27b7e320e1af5f
Will Pragnell <wpragnell@gmail.com>
2016-04-19 12:06:27 +0100

    minion talks in its sleep

--- src/minion/main.go
+++ src/minion/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "time"
+import (
+	"os"
+	"time"
+)
 
 func main() {
+	os.Stdout.WriteString("hello stdout\n")
+	os.Stderr.WriteString("hello stderr\n")
+
 	for {
 		time.Sleep(time.Minute)
 	}

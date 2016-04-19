9b995df6841576c8f8c5f67adc6149b3eee913bd
Will Pragnell <wpragnell@gmail.com>
2016-04-19 16:50:30 +0100

    minion does things for you

--- src/minion/main.go
+++ src/minion/main.go
@@ -1,15 +1,60 @@
 package main
 
 import (
-	"os"
-	"time"
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+	"syscall"
 )
 
-func main() {
-	os.Stdout.WriteString("hello stdout\n")
-	os.Stderr.WriteString("hello stderr\n")
+var port = flag.Int("port", 8080, "port to listen on")
 
-	for {
-		time.Sleep(time.Minute)
-	}
+func main() {
+	flag.Parse()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		cmdJson := struct {
+			Path string `json:"path"`
+		}{}
+
+		if err := json.NewDecoder(r.Body).Decode(&cmdJson); err != nil {
+			http.Error(w, "unable to parse request json", http.StatusInternalServerError)
+			return
+		}
+
+		cmd := exec.Command(cmdJson.Path)
+		var stdout, stderr bytes.Buffer
+		cmd.Stdout = &stdout
+		cmd.Stderr = &stderr
+		exitStatus := 0 // i'm an optimist
+
+		err := cmd.Run()
+		if err != nil {
+			switch e := err.(type) {
+			case *exec.ExitError: // command exited non-zero
+				sys := e.Sys().(syscall.WaitStatus)
+				exitStatus = sys.ExitStatus()
+			default: // I/O error or something?
+				http.Error(w, "failed to execute command: "+e.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		responseJson := struct {
+			Stdout     string
+			Stderr     string
+			ExitStatus int
+		}{
+			Stdout:     stdout.String(),
+			Stderr:     stderr.String(),
+			ExitStatus: exitStatus,
+		}
+		json.NewEncoder(w).Encode(responseJson)
+	})
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
